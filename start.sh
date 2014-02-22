eb0e17cfbda864233dd91852e4902a0684afa102
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-02-22 15:09:53 +0100

    Improved start script

--- start.sh
+++ start.sh
@@ -3,11 +3,26 @@
 set -e
 set -u
 
+function shutdown {
+	echo "Shutting down..."
+
+	if $(kill -s 0 $python_pid &>/dev/null) ; then
+		kill $python_pid
+	fi
+	if $(kill -s 0 $node_pid &>/dev/null) ; then
+		kill $node_pid
+	fi
+	if $(kill -s 0 $vbam_pid &>/dev/null) ; then
+		kill -9 $vbam_pid # :-(
+	fi
+}
+trap shutdown 2
+
 cd "$(dirname "$0")" # cwd will be directory of current script
 
 vbam emerald.gba &>/dev/null &
 vbam_pid=$!
-echo "Please press enter when ready to select window"
+echo "Press enter when ready to select window"
 read
 
 node backend/nodejs/main.js >/dev/null &
@@ -18,5 +33,4 @@ python_pid=$!
 
 echo "Press enter to end this application"
 read
-kill $python_pid $node_pid
-kill -9 $vbam_pid # :-(
+shutdown

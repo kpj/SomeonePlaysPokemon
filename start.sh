aa4dc1d4d532d47faf550ffa3d90945a00799124
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-02-22 02:38:14 +0100

    Improved start-script

--- start.sh
+++ start.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-vbam emerald.gba &
-sleep 2
+set -e
+set -u
+
+cd "$(dirname "$0")" # cwd will be directory of current script
+
+vbam emerald.gba &>/dev/null &
+vbam_pid=$!
+echo "Please press enter when ready to select a window"
+read # wait for user to select window
+
 node frontend/main.js &
-python3 backend/glue.py
-kill %1
-killall -9 vbam
+node_pid=$!
+
+python3 backend/glue.py >/dev/null &
+python_pid=$!
+
+echo "Press enter to end this application"
+read
+kill $python_pid $node_pid
+kill -9 $vbam_pid # :-(
